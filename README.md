1469302944a5426144701d0eb5a78e02a7fd00bb
laciechang <laciechang@163.com>
2021-05-05 02:01:00 +0800

    create files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Resolve-render-notification-to-Dingding
+渲染完成后自动发送到钉钉机器人进行通知
+
+# 安装
+- 请将 Dingding_notification.py 拷贝至达芬奇指定的脚本存放目录下
+- macOS: /Users/{你的用户名}/Library/Application Support/Blackmagic Design/DaVinci Resolve/Fusion/Scripts/Deliver
+- Windows: C:\Users{你的用户名}\AppData\Roaming\Blackmagic Design\DaVinci Resolve\Support\Fusion\Scripts\Deliver
+- 请使用v17及以上版本
+- 在渲染设置中即可找到
+- 仅支持在达芬奇内使用，不支持在外部运行
+
+# 用法
+- 新建渲染任务时，勾选上 Trigger script at 【End】 of render job之后，选择该脚本即可，并会在渲染任务完成后自动执行
+
+
+# 需要
+- Python 3.6 64-bit
+- 第三方库：requests
+- 其他版本的 Python 达芬奇并不支持
---
+++ dingding_notification.py
@@ -0,0 +1,92 @@
+# -*- coding:utf-8 -*-
+# Author: 张来吃
+# Version: 1.0
+# Contact: laciechang@163.com
+
+# -----------------------------------------------------
+# 请注意：该脚本仅可在达芬奇17及以上版本内部运行
+# 请先确保下方涉及的库可用
+# -----------------------------------------------------
+
+import json
+import requests
+import math
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+
+# ---------------------------------------------------------------
+
+# 请在下方填写你的钉钉机器人信息，注意保留引号
+
+sec_code = "***" # 通常为SEC开头的一串代码
+webhook_url = "***" # 通常为https开头的一个url地址
+
+# ---------------------------------------------------------------
+
+def send_dd(content, title):
+    sign = "&timestamp={0}&sign={1}".format(sec()[0], sec()[1])
+    webhook = webhook_url
+    url = webhook + sign
+    header = {"Content-Type":"application/json"}
+    data = {"msgtype":"markdown",  "markdown":{"title": "%s" % title,"text":"%s" % content},  
+        "at":{  "atMobiles":[], 
+        "isAtAll": False  
+        }
+    } 
+    sendData = json.dumps(data)
+    response = requests.request("POST", url, data=sendData, headers=header)
+    print(response.text)
+
+def sec():
+    timestamp = str(round(time.time() * 1000))
+    secret = sec_code
+    secret_enc = secret.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    return timestamp, sign
+
+def getJobDetailsBasedOnId(project, jobId):
+    jobList = project.GetRenderJobList()
+    for jobDetail in jobList:
+        if jobDetail["JobId"] == jobId:
+            return jobDetail
+    return ""
+
+def reformatJobDetail(detail):
+    o = "- 任务名称：{0}\n".format(str(detail['RenderJobName']))
+    o += "- 输出路径：{0}\n".format(str(detail['TargetDir']))
+    o += "- 文件名：{0}\n".format(str(detail['OutputFilename']))
+    o += "- 输出分辨率：{0}x{1}\n".format(str(detail['FormatWidth']), str(detail['FormatHeight']))
+    return o
+
+def reformatJobStatus(status):
+    time = int(math.floor(int(status['TimeTakenToRenderInMs'])/1000))
+
+    o = "- 当前状态：{0}\n".format(str(status['JobStatus']))
+    o += "- 完成比例：{0}%\n".format(str(status['CompletionPercentage']))
+    o += "- 耗时：{0}时{1}分{2}秒\n".format(int(math.floor(time/3600)), int(math.floor(time/60)), time)
+    return o
+
+def main():
+    project = bmd.scriptapp('Resolve').GetProjectManager().GetCurrentProject()
+    project_name = project.GetName()
+    detailstatus = project.GetRenderJobStatus(job)
+
+    title = "\"{0}\"的渲染报告: {1}".format(project_name, status)
+    content = ""
+
+    if len(str(error)) >= 1:
+        content += "## 错误信息: \n\"{0}\"\n".format(error)
+
+    content += "## 执行情况:\n{0}\n".format(reformatJobStatus(detailstatus))
+    content += "## 任务细节: \n{0}".format(reformatJobDetail(getJobDetailsBasedOnId(project, job)))
+    
+    send_dd(content, title)
+
+if __name__ == '__main__':
+    main()
